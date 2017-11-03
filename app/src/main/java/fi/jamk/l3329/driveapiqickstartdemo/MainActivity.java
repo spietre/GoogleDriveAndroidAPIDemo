364067dc1ebec3219ffc699f0046c6b24df1f423
Peter Seliga <seliga.peter5@gmail.com>
2017-11-03 16:24:12 +0200

    added project

---
+++ app/src/main/java/fi/jamk/l3329/driveapiqickstartdemo/MainActivity.java
@@ -0,0 +1,222 @@
+package fi.jamk.l3329.driveapiqickstartdemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.MetadataChangeSet;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_CODE_CAPTURE_IMAGE = 1;
+    private static final int REQUEST_CODE_CREATOR = 2;
+    private static final int REQUEST_CODE_RESOLUTION = 3;
+    private boolean isAPIConnected;
+    private GoogleApiClient mGoogleApiClient;
+    private Bitmap mBitmapToSave;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialize Google Drive API Client!
+        connectAPIClient();
+
+        findViewById(R.id.btnTakePhoto).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Start camera to take a picture
+                if(isAPIConnected) {
+                    startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE), REQUEST_CODE_CAPTURE_IMAGE);
+                }else{
+                    Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "Error Google API is disable or permissions are required!", Snackbar.LENGTH_LONG)
+                            .setActionTextColor(Color.RED);
+
+                    View snackbarView = snackbar.getView();
+                    snackbarView.setBackgroundColor(Color.DKGRAY);
+                    TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+                    textView.setTextColor(Color.RED);
+                    snackbar.show();
+
+                }
+            }
+        });
+    }
+
+
+    //Create a new file and save it to Drive.
+    private void saveFileToDrive() {
+        // Start by creating a new contents, and setting a callback.
+        Log.i(TAG, "saveFileToDrive() Creating new content.");
+        final Bitmap image = mBitmapToSave;
+
+        Drive.DriveApi.newDriveContents(mGoogleApiClient)
+                .setResultCallback(
+                        new ResultCallback<DriveApi.DriveContentsResult>() {
+                            @Override
+                            public void onResult(DriveApi.DriveContentsResult result) {
+
+                                    // If the operation wasn't successful, return
+                                    if (!result.getStatus().isSuccess()) {
+                                        Log.e(TAG, "Failed to create new content!.");
+                                        return;
+                                    }
+                                    Log.i(TAG, "New content has been created.");
+                                    // Get an output stream for the contents.
+                                    OutputStream outputStream = result.getDriveContents().getOutputStream();
+                                    // Write the bitmap data from it.
+                                    ByteArrayOutputStream bitmapStream = new ByteArrayOutputStream();
+                                    image.compress(Bitmap.CompressFormat.PNG, 100, bitmapStream);
+                                    try {
+                                        outputStream.write(bitmapStream.toByteArray());
+                                    } catch (IOException e1) {
+                                        Log.i(TAG, "Unable to write file contents.");
+                                    }
+                                    // Create the initial metadata - MIME type and title.
+                                    // Note that the user will be able to change the title later.
+                                    MetadataChangeSet metadataChangeSet = new MetadataChangeSet.Builder()
+                                            .setMimeType("image/jpeg").setTitle("myPhoto.png").build();
+                                    // Create an intent for the file chooser, and start it.
+                                    IntentSender intentSender = Drive.DriveApi
+                                            .newCreateFileActivityBuilder()
+                                            .setInitialMetadata(metadataChangeSet)
+                                            .setInitialDriveContents(result.getDriveContents())
+                                            .build(mGoogleApiClient);
+                                    try {
+                                        startIntentSenderForResult(
+                                                intentSender, REQUEST_CODE_CREATOR, null, 0, 0, 0);
+                                    } catch (IntentSender.SendIntentException e) {
+                                        Log.i(TAG, "Failed to launch file chooser.");
+                                    }
+                            }
+                        }
+                );
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onPause();
+    }
+
+    //Disconnect only when the application is closed!
+    @Override
+    protected void onDestroy() {
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        switch (requestCode) {
+            case REQUEST_CODE_CAPTURE_IMAGE:
+                // Called after a photo has been taken.
+                if (resultCode == Activity.RESULT_OK) {
+                    // Store the image data as a bitmap for writing later.
+                    mBitmapToSave = (Bitmap) data.getExtras().get("data");
+                    saveFileToDrive();
+                }
+                break;
+            case REQUEST_CODE_CREATOR:
+                //Called after a file is saved to Drive.
+                if (resultCode == RESULT_OK) { //succesfully saved!.
+                    Log.i(TAG, "Image successfully saved.");
+                    mBitmapToSave = null;
+                    Snackbar snackbar =  Snackbar.make(findViewById(android.R.id.content), "Photo succesfully saved to Google Drive!", Snackbar.LENGTH_LONG)
+                            .setActionTextColor(Color.RED);
+
+                    View snackbarView = snackbar.getView();
+                    snackbarView.setBackgroundColor(Color.DKGRAY);
+                    TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+                    textView.setTextColor(Color.YELLOW);
+                    snackbar.show();
+
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "GoogleApiClient connection failed: " + result.toString());
+        isAPIConnected = false;
+        if (!result.hasResolution()) {
+            // show the localized error dialog.
+            GoogleApiAvailability.getInstance().getErrorDialog(this, result.getErrorCode(), 0).show();
+            return;
+        }
+        // Called typically when the app is not yet authorized, and authorization dialog is displayed to the user.
+        try {
+            result.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
+        } catch (IntentSender.SendIntentException e) {
+            Log.e(TAG, "Exception while starting resolution activity. " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.i(TAG, "* API client connected !!!.");
+        isAPIConnected = true;
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.i(TAG, "GoogleApiClient connection suspended");
+    }
+
+    private void connectAPIClient(){
+        if (mGoogleApiClient == null) {
+            Log.i(TAG, "connectAPIClient().");
+            // Create the API client and bind it to an instance variable.
+            // We use this instance as the callback for connection and connection
+            // failures.
+            // Since no account name is passed, the user is prompted to choose.
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Drive.API)
+                    .addScope(Drive.SCOPE_FILE)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+        // Connect the client. Once connected, the camera is launched.
+        mGoogleApiClient.connect();
+    }
+}
